19c6deafa527dea88f87b0314a4786e167bfc5a0
Eric Meas <emmeas@gmail.com>
2019-06-17 16:48:09 -0700

    Update spell_that_word.py
    added import re and print command for word that needs to be spelled

--- spell_that_word.py
+++ spell_that_word.py
@@ -1,10 +1,14 @@
 #spell_that_word.py: Simple spelling text game
 
+import re
+
 from random import randint
 
 words_store = ["cat","dog","mouse","bird","human","wolf"]
 rand_num = randint(0,5)
 
 words_rand = words_store[rand_num]
+
+print(words_rand)
 
 user_input = input("Type the word you see! ")
