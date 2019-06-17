3255e2f9132960cca353a1b06c4c147f76ad8669
Eric Meas <emmeas@gmail.com>
2019-06-17 16:52:57 -0700

    Update spell_that_word.py
    fixed some spacing so it looks better and added re if else statement to make module functional

--- spell_that_word.py
+++ spell_that_word.py
@@ -1,14 +1,17 @@
 #spell_that_word.py: Simple spelling text game
 
 import re
-
 from random import randint
 
 words_store = ["cat","dog","mouse","bird","human","wolf"]
 rand_num = randint(0,5)
-
 words_rand = words_store[rand_num]
 
 print(words_rand)
 
 user_input = input("Type the word you see! ")
+
+if re.match(words_rand, user_input):
+  print("Correct!")
+else:
+  print("Incorrect!")
